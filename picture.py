aeb59021481ffe59f2778bdaf52c5859205f9f89
st18006me <70875415+st18006me@users.noreply.github.com>
2023-03-06 00:11:04 +0900

    Create picture.py

---
+++ picture.py
@@ -0,0 +1,20 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier(r'haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #顔の学習データ
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=3)
+
+    cv2.imshow('image', img)
+    key = cv2.waitKey(1)
+    if key == 32:   #スペース
+        cv2.imwrite('makeup/sample.jpg', img)
+    if key == 27:   #escキー
+        break
+ 
+cap.release()
+cv2.destroyAllWindows()
